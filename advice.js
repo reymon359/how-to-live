4844343a6c3842b64c6799f99def589040caa495
reymon359 <ramonmorcillocascales@gmail.com>
2020-08-27 16:04:11 +0200

    refactor: use spaces instead of tabs

--- advice.js
+++ advice.js
@@ -1,14 +1,14 @@
 [
-	{
-		id: 'example-id',
-		title: 'example-title',
-		description: 'example-description',
-		content: 'example-content',
-		quotes: ['example-quote'],
-		learnedFrom: 'example-learnedFrom',
-		usefulResources: ['example-usefulResource'],
-		relatedAdvice: ['example-relatedAdvice'],
-		topics: ['example-topic'],
-		tags: ['example-tag'],
-	},
+  {
+    id: 'example-id',
+    title: 'example-title',
+    description: 'example-description',
+    content: 'example-content',
+    quotes: ['example-quote'],
+    learnedFrom: 'example-learnedFrom',
+    usefulResources: ['example-usefulResource'],
+    relatedAdvice: ['example-relatedAdvice'],
+    topics: ['example-topic'],
+    tags: ['example-tag'],
+  },
 ];
