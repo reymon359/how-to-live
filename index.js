ac066b3e10a42ae319c58e73f49217485e0db292
reymon359 <ramonmorcillocascales@gmail.com>
2021-04-07 08:43:31 +0200

    feat: trying to use collman

---
+++ index.js
@@ -0,0 +1,16 @@
+const collman = require('collman');
+
+const config = {
+  inputType: 'markdown',
+  outputType: 'markdown',
+  pathRootDirectory: '/Users/ramonmorcillo/Documents/developer/how-to-live',
+  inputDirectory: 'advices',
+  outputDirectory: 'docs',
+  docsify: true
+}
+
+const  main = () => {
+ collman({...config})
+}
+
+main()
