35ac47e323a789b713c56d07c415a8b2262d442d
reymon359 <ramonmorcillocascales@gmail.com>
2021-04-18 10:46:56 +0200

    refactor: update collman config

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 const collman = require('collman');
 
-const config = {
-  inputType: 'markdown',
-  outputType: 'markdown',
-  pathRootDirectory: '/Users/ramonmorcillo/Documents/developer/how-to-live/',
-  inputDirectory: 'advices',
-  outputDirectory: 'docs',
-  docsify: true
-}
-
-const  main = () => {
-  collman(config)
-}
-
-main()
+collman({ inputDirectory: 'advices' })
