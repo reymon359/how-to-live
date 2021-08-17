0e23a4cbc5e99cbaf37166d1af9386a315958480
reymon359 <ramonmorcillocascales@gmail.com>
2021-08-17 22:36:56 +0200

    feat: add repo to docsify config

--- docsify.config.js
+++ docsify.config.js
@@ -2,6 +2,7 @@ module.exports = {
   main: {
     name: 'How to live',
     basePath: 'https://raw.githubusercontent.com/reymon359/how-to-live/master/docs/', // To work with GitHub Pages
+    repo: 'https://github.com/reymon359/how-to-live',
     loadSidebar: true,
     alias: {
       '/.*/_sidebar.md': '/_sidebar.md'
